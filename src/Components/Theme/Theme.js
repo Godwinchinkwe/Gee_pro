18bf27401059190fab55714d757637c4f8396ec1
godwinchinkwe <chinkwegodwin@gmail.com>
2024-05-01 14:44:40 +0100

    theme color

--- src/Components/Theme/Theme.js
+++ src/Components/Theme/Theme.js
@@ -2,10 +2,38 @@ import React from 'react'
 import "./Theme.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react'
+
+
+const getLocalStorageTheme = () =>{
+  let theme = 'light-theme';
+
+  if (localStorage.getItem('theme')){
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
+}
 
 const Theme = () => {
+
+  const [Theme, setTheme] = useState(getLocalStorageTheme());
+
+  let toggleTheme=()=>{
+    if(Theme === 'light-theme'){
+      setTheme('dark-theme')
+    }else{
+      setTheme('light-theme')
+    }
+  }
+
+  useEffect(()=>{
+    document.documentElement.className = Theme;
+    localStorage.setItem('theme', Theme)
+  }, [Theme])
+
+  
   return (
-    <span className='Bg-toggler'>
+    <span className='Bg-toggler' onClick={toggleTheme}>
       <FontAwesomeIcon icon={faMoon} />
     </span>
   )
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import cv from '../../assets/CV.pdf'
+import HeadingText from "../../Components/HeadingText/HeadingText"
+import "./About.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
 
 const About = () => {
   return (
-    <div>
-      about
-    </div>
+    <>
+    <section className='About'>
+      <HeadingText title={'About'} titlePrimary={'Me'}/>
+
+      <div className='container about-container'>
+        <div className='about-left'>
+          <h3> I'm Godwin Chinkwe</h3>
+          <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+          <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+          <a href={cv} download className='btn'>
+            Download CV <span> <FontAwesomeIcon icon={faDownload}/> </span>
+          </a>
+        </div>
+        <div className='about-right'>
+          
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
